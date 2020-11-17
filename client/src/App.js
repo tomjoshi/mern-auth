719aab0d1802522527911024deb991141b7c1a49
Tom Joshi <tomjoshi7@gmail.com>
2020-11-17 09:19:24 -0800

    Finished Login.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
+// Remember to push code to github everyday
+// FInal exam posted 11/24, project 2 instead of quiz 2  
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
-    )
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    );
   }
 }
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -48,9 +48,36 @@ class Login extends Component {
                                 </Link>
                             </p>
                         </div>
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.email}
+                                    error={errors.email}
+                                    id="email"
+                                    type="email"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="col s12" style={{paddingLeft:"11.250px"}}>
+                                <button
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+                                        Login
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
+
+export default Login;
