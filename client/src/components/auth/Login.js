5c142c1af68371603be8243c0a8853f21b047c83
Tom Joshi <tomjoshi7@gmail.com>
2020-11-16 17:53:40 -0800

    Half done with Login.js

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            errors: {}
+        };
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        console.log(userData);
+    };
+
+    render() { 
+        const { errors } = this.state;
+        return (
+            <div className="container">
+                <div style={{ marginTop: "4rem"}} className="row">
+                    {/* Play around with this col and figure out what it means */}
+                    <div className="col s8 offset-s2">
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons left">
+                                keyboard_backspace
+                            </i>
+                            Back to home
+                        </Link>
+                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                            <h4>
+                                <b>Login</b> below
+                            </h4>
+                            <p className="grey-text text-darken-1">
+                                Don't have an account? 
+                                <Link to="/register">
+                                    Register
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
