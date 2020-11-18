2cf78b06e2de93c072c543a591804754dd700f22
Tom Joshi <tomjoshi7@gmail.com>
2020-11-18 09:22:39 -0800

    Completed Navbar, Landing, Login, and Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-
-// Remember to push code to github everyday
-// FInal exam posted 11/24, project 2 instead of quiz 2  
 class App extends Component {
   render() {
     return (
