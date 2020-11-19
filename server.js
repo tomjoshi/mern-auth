ee358e9843f53a4059375455634927dbbae72262
Tom Joshi <tomjoshi7@gmail.com>
2020-11-19 09:44:40 -0800

    Changed server.js

--- server.js
+++ server.js
@@ -50,4 +50,6 @@ app.use("/api/users/", users);
 // process.env.port is Heroku's port if you choose to deploy the app there
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server up and running on port ${port} !"));
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
