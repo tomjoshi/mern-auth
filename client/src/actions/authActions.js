7f7def20fe6b65c631137c278bbcad09b27b131b
Tom Joshi <tomjoshi7@gmail.com>
2020-11-24 07:48:37 -0800

    Added register user in authActions.js

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -11,5 +11,22 @@ import {
 // Register user
 export const registerUser = (userData, history) => dispatch => {
     axios.post("/api/users/register", userData)
-    
+        .then(res => history.push("/login")) // redirect to login on successfull register
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+// Login - get user token
+export const loginUser = userData => dispatch => {
+    axios
+        .post("/api/users/login", userData)
+        .then(res => {
+            // Save to localStorage
+            // Set token to localStorage
+            
+        })
 }
