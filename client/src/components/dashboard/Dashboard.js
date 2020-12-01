f3dfe1f557cf4f49df1bb7deb3878b6a116ca3fc
Tom Joshi <tomjoshi7@gmail.com>
2020-12-01 08:38:49 -0800

    Made additions to  Dashboard.js

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+
+class Dashboard extends Component {
+    onLogoutClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+    };
+
+    render() {
+        const { user } = this.props.auth;
+
+        return (
+            <div style={{ height: "75vh" }} className="container valign-wrapper">
+                <div className="row">
+                    <div className="col s12 center-align">
+                        <h4>
+                            <b>Hey there, </b> { user.name.split(" ")[0]}
+                            <p className="flow-text grey-text text-darken-1">
+                                You are logged into a full-stack{" "}
+                                <span style={{ fontFamily: "monospace" }}>MERN</span> app👏
+                            </p>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
