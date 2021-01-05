4862a45c23e80442edb7116c88f7a09660b0ebb7
Tom Joshi <tomjoshi@Toms-MacBook-Pro.local>
2021-01-05 10:30:36 -0800

    Added post transactions route to plaid

--- routes/api/plaid.js
+++ routes/api/plaid.js
@@ -24,16 +24,56 @@ var ACCESS_TOKEN = null;
 var ITEM_ID = null;
 
 // Routes
+
 // @route POST api/plaid/accounts/add
 // @desc Trades public token for access token and stores credentials in database
 // @access Private
+
+
+// @route DELETE api/plaid/accounts/:id
+// @desc Delete account with given id
+// @access Private
+
+
+// @route GET api/plaid/accounts
+// @desc Get all accounts linked with plaid for a specific user
+// @access Private
+
+// @route POST api/plaid/accounts/transactions
+// @desc Fetch transactions from past 30 days from all linked accounts
+// @access Private
 router.post(
-    "/accounts/add",
+    "/accounts/transactions",
     passport.authenticate("jwt", { session: false }),
     (req, res) => { 
-        
+        const now = moment()
+        const today = now.format("YYYY-MM-DD");
+        const thirtyDaysAgo = now.subtract(30, "days").format("YYY-MM-DD");
+        let transactions = []
+        const accounts = req.body;
+
+        if (account) { 
+            accounts.forEach(function(acount) {
+                ACCESS_TOKEN = account.accessToken;
+                const institutionName = account.institutionName;
+                client
+                    .getTransactions(ACCESS_TOKEN, thirtyDaysAgo, today)
+                    .then(response => {
+                        transactions.push({
+                            accountName: institutionName,
+                            transactions: response.transactions
+                        });
+                        // Wait until all transactions have been added, then send response
+                        if (transactions.length === accounts.length) { 
+                            res.join(transactions);
+                        }
+                    })
+                    .catch(err => console.log(err));
+
+            });
+        }
     }
-)
+);
 
 
 module.exports = router;
