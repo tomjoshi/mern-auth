34237846d879125a656cc74a9689fc0e5b665690
Tom Joshi <tomjoshi@Toms-MacBook-Pro.local>
2021-01-07 11:22:38 -0800

    Completed the accounts add route

--- routes/api/plaid.js
+++ routes/api/plaid.js
@@ -28,7 +28,47 @@ var ITEM_ID = null;
 // @route POST api/plaid/accounts/add
 // @desc Trades public token for access token and stores credentials in database
 // @access Private
+router.post(
+    "/accounts/add",
+    passport.authenticate("jwt", {session: false }),
+    (req, res) => {
+        PUBLIC_TOKEN = req.body.public_token;
+        const userId = req.user.id;
+        const institution = req.body.metadata.institution;
+        const { name, instituion_id } = institution;
 
+        if (PUBLIC_TOKEN) {
+            client
+                .exchangePublicToken(PUBLIC_TOKEN)
+                .then(exchangeResponse => {
+                    ACCESS_TOKEN = exchangeResponse.access_token;
+                    ITEM_ID = exchangeReponse.item_id;
+                    // Check if account already exists for a specific user
+                    Account.findOne({
+                        user: req.user.id,
+                        institutionId: instituion_id
+                    })
+                    .then(account => {
+                        if (account) {
+                            console.log("Account already exists");
+                        }
+                        else {
+                            const newAccount = new Account({
+                                userId: userId,
+                                accessToken: ACCESS_TOKEN,
+                                itemId: ITEM_ID,
+                                institutionId: institution_id,
+                                institutionName: name
+                            });
+                            newAccount.save().then(account => res.json(account));
+                        }
+                    })
+                    .catch(err => console.log(err)); // Mongo Error
+                })
+                .catch(err => console.log(err)); // Plaid Error
+        }
+    }
+)
 
 // @route DELETE api/plaid/accounts/:id
 // @desc Delete account with given id
