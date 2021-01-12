718c6518560776dfd2fc3085e6fd44a176013819
Tom Joshi <tomjoshi@Toms-MacBook-Pro.local>
2021-01-12 07:54:08 -0800

    Create redux action for account

---
+++ client/src/actions/accountActions.js
@@ -0,0 +1,13 @@
+// Promise based HTTP client
+import axios from "axios";
+
+import {
+    ADD_ACCOUNT,
+    DELETE_ACCOUNT,
+    GET_ACCOUNTS,
+    ACCOUNTS_LOADING,
+    GET_TRANSACTIONS,
+    TRANSACTIONS_LOADING
+} from "./types";
+
+// Actions will go here
