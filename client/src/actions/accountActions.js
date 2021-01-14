28a4c93cb99417eb7a72cdf7a466fd16f769adaa
Tom Joshi <tomjoshi@Toms-MacBook-Pro.local>
2021-01-13 16:22:04 -0800

    Account actions: add

--- client/src/actions/accountActions.js
+++ client/src/actions/accountActions.js
@@ -11,3 +11,21 @@ import {
 } from "./types";
 
 // Actions will go here
+
+// Add account
+export const addAccount = plaidData => dispatch => { 
+    const accounts = plaidData.accounts;
+    axios
+        .post("/api/plaid/accounts/add", plaidData)
+        .then(res =>
+            dispatch({
+                type: ADD_ACCOUNT,
+                payload: res.data
+            })
+        )
+        .then(data =>
+            accounts ?
+            dispatch(getTransactions(accounts.concat(data.payload))) : null
+        )
+        .catch(err => console.log(err));
+};
