aab55ec6ae321e0d7ef80a2946b060b8dc71ae69
Tom Joshi <tomjoshi@Toms-MacBook-Pro.local>
2021-01-16 09:14:33 -0800

    Get accounts for specific users and loading account actions

--- client/src/actions/accountActions.js
+++ client/src/actions/accountActions.js
@@ -49,3 +49,29 @@ export const deleteAccount = plaidData => dispatch => {
             .catch(err => console.log(err));
     }
 }
+
+// Get all accounts for specific user
+export const getAccounts = () => dispatch => {
+    dispatch(setAccountsLoading());
+    axios
+        .get("/api/plaid/accounts")
+        .then(res => 
+            dispatch({
+                type: GET_ACCOUNTS,
+                payload: res.data
+            })
+        )
+        .catch(err => 
+            dispatch({
+                type: GET_ACCOUNTS,
+                payload: null
+            })
+        );
+};
+
+// Accounts loading
+export const setAccountsLoading = () => {
+    return {
+        type: ACCOUNTS_LOADING
+    };
+};
