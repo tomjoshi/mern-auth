c829c6be018d16eb9f12cb7f5439c904778babc1
Tom Joshi <tomjoshi@Toms-MacBook-Pro.local>
2021-01-17 10:03:09 -0800

    Created a account reducer

---
+++ client/src/reducers/accountReducer.js
@@ -0,0 +1,14 @@
+/*
+The file will do two things: 
+1) Define initial state
+2) Define state change based on which actions called
+*/
+
+import {
+    ADD_ACCOUNT,
+    DELETE_ACCOUNT,
+    GET_ACCOUNT, 
+    ACCOUNTS_LOADING,
+    GET_TRANSACTIONS, 
+    TRANSACTIONS_LOADING
+} from "../actions/types";
