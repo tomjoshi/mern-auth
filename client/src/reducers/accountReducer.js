e79060cb637f7f7648fc3d5249a8a3806eb16c71
Tom Joshi <tomjoshi@Toms-MacBook-Pro.local>
2021-01-19 10:21:18 -0800

    Defined state based on which actions called

--- client/src/reducers/accountReducer.js
+++ client/src/reducers/accountReducer.js
@@ -27,5 +27,36 @@ export default function(state = initialState, action) {
                 ...state,
                 accountsLoading: true
             };
+        case ADD_ACCOUNT:
+            return {
+                ...state,
+                accounts: [action.payload, ...state.accounts]
+            };
+        case DELETE_ACCOUNT:
+            return {
+                ...state,
+                accounts: state.accounts.filter(
+                    account => account._id !== action.payload
+                )
+            };
+        case GET_ACCOUNTS: 
+            return { 
+                ...state,
+                accounts: action.payload,
+                accountsLoading: false
+            };
+        case TRANSACTIONS_LOADING:
+            return {
+                ...state,
+                transactionsLoading: true
+            };
+        case GET_TRANSACTIONS:
+            return {
+                ...state,
+                transactions: action.payload,
+                transactionsLoading: false
+            };
+        default: 
+        return state;
     }
 }
